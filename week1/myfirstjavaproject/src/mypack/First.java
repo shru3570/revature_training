6fd8af4918cd1d01de07de6393af5465fbf082a4
shru3570 <khedkarshruti411@gmail.com>
2021-07-27 16:04:18 +0530

    FirstJavaProgram

---
+++ week1/myfirstjavaproject/src/mypack/First.java
@@ -0,0 +1,12 @@
+package mypack;
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	  System.out.println("its my first java program");   
+		
+
+	}
+
+}
